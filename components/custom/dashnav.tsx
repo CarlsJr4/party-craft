d151bb0ba8b1745e9f6973c17eeb526c29d75eb1
Carl Dungca <carl.dungca@yahoo.com>
2023-11-07 01:29:07 -0800

    feature: adds routing to dashnav component

--- components/custom/dashnav.tsx
+++ components/custom/dashnav.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const DashNav = () => {
   return (
     <div className="col-span-1 p-4 bg-slate-200 flex flex-col gap-6">
       <Button className="mt-3">Create New +</Button>
-      <p>Upcoming</p>
-      <p>My Events</p>
-      <p>Past Events</p>
-      <p>Drafts</p>
+      <Link href="/dashboard/upcoming">Upcoming</Link>
+      <Link href="/dashboard/">My Events</Link>
+      <Link href="/dashboard/past">Past Events</Link>
+      <Link href="/dashboard/drafts">Drafts</Link>
     </div>
   );
 };
