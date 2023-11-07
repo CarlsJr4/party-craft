a3f1f39491b5c43db98ab65645c629a031905680
Carl Dungca <carl.dungca@yahoo.com>
2023-11-06 23:56:41 -0800

    Config: Configures vitest to use react plugin and jsdom environment

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './') }],
+  },
+  test: {
+    environment: 'jsdom',
+  },
+});
