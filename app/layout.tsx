ec289a0bf4bbcb73b305b4511fe16db61e83e7b7
Carl Dungca <carl.dungca@yahoo.com>
2023-11-10 19:18:08 -0800

    refactor: changes div to main in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <div>{children}</div>
+        <main>{children}</main>
       </body>
     </html>
   );
