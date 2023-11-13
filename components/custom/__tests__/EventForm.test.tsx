8394e1782a56ed89133e18865ee88c36245bc89e
Carl Dungca <carl.dungca@yahoo.com>
2023-11-13 00:24:28 -0800

    refactor: adds reusable setup and form fill logic in EventForm.test.tsx

--- components/custom/__tests__/EventForm.test.tsx
+++ components/custom/__tests__/EventForm.test.tsx
@@ -5,22 +5,43 @@ import { Toaster } from '@/components/ui/toaster';
 import EventForm from '@/components/custom/EventForm';
 
 const setup = () => {
-  const utils = render(<EventForm />);
+  render(<EventForm />);
   render(<Toaster />);
-  const submitButton = screen.getByText(/Submit/i);
+
   const user = userEvent.setup();
+
+  const submitButton = screen.getByText(/Submit/i);
+  const fields = {
+    eventNameField: screen.getByLabelText('Event name'),
+    eventDescField: screen.getByLabelText('Event description'),
+    eventDateField: screen.getByLabelText('Event date'),
+  };
 
   return {
     submitButton,
     user,
-    ...utils,
+    ...fields,
   };
+};
+
+const submitValidForm = async () => {
+  // NOTE: submitValidForm may cause double-render issues if setup and submitValidForm are run in the same block
+  const { user, eventNameField, eventDescField, eventDateField, submitButton } =
+    setup();
+  await user.click(eventNameField);
+  await user.keyboard('Ice skating with friends');
+  await user.click(eventDescField);
+  await user.keyboard('Meet up at the mall and go to the ice skating rink');
+  await user.click(eventDateField);
+  await user.keyboard('{Enter}'); // Simulates selecting the next day in the calendar component
+  await user.click(submitButton);
 };
 
 describe('Isolated event form validation', () => {
   it('Displays errors when empty fields are submitted', async () => {
     const { submitButton, user } = setup();
     await user.click(submitButton);
+
     const errEventNameRequired = screen.getByText(
       /Event name must be at least 3 characters./i
     );
@@ -28,21 +49,21 @@ describe('Isolated event form validation', () => {
       /Event description cannot be blank./i
     );
     const errDateRequired = screen.getByText(/Please pick a date./i);
+
     const errors = [
       errEventNameRequired,
       errEventDescRequired,
       errDateRequired,
     ];
+
     errors.forEach(error => {
       expect(error).toBeInTheDocument();
     });
   });
 
   it('Displays errors when a one-letter event name is submitted', async () => {
-    const { submitButton, user } = setup();
-    const eventnameField = screen.getByLabelText('Event name');
-    expect(eventnameField).toBeInTheDocument();
-    await user.click(eventnameField);
+    const { submitButton, user, eventNameField } = setup();
+    await user.click(eventNameField);
     await user.keyboard('a');
     await user.click(submitButton);
     const errorMessage = screen.getByText(
@@ -61,47 +82,27 @@ describe('Isolated eventForm submission', () => {
   });
 
   it('Displays success toast when a form is submitted without errors', async () => {
-    const { submitButton, user } = setup();
-    // Submit all the form fields
-    const eventnameField = screen.getByLabelText('Event name');
-    const eventdescField = screen.getByLabelText('Event description');
-    const eventdateField = screen.getByLabelText('Event date');
-    await user.click(eventnameField);
-    await user.keyboard('Ice skating with friends');
-    await user.click(eventdescField);
-    await user.keyboard('Meet up at the mall and go to the ice skating rink');
-    await user.click(eventdateField);
-    await user.keyboard('{Enter}'); // Simulates selecting the next day in the calendar component
-    await user.click(submitButton);
+    await submitValidForm();
     const successMessage = await screen.findByText('Event created!');
     expect(successMessage).toBeInTheDocument();
   });
 
   it('Keeps the form values filled after an usuccessful submission', async () => {
-    const { submitButton, user } = setup();
-    const eventnameField = screen.getByLabelText('Event name');
-    await user.click(eventnameField);
-    await user.keyboard('I');
+    const { submitButton, user, eventNameField } = setup();
+    await user.click(eventNameField);
+    await user.keyboard('a');
     await user.click(submitButton);
-    expect(eventnameField).toHaveValue('I');
+    const errorMessage = screen.getByText(
+      /Event name must be at least 3 characters./i
+    );
+    expect(eventNameField).toHaveValue('a');
+    expect(errorMessage).toBeInTheDocument();
   });
 
   it('Resets the form after a successful submission', async () => {
-    const { submitButton, user } = setup();
-    // Submit all the form fields
-    const eventnameField = screen.getByLabelText('Event name');
-    const eventdescField = screen.getByLabelText('Event description');
-    const eventdateField = screen.getByLabelText('Event date');
-
-    const formFields = [eventnameField, eventdescField, eventdateField];
-
-    await user.click(eventnameField);
-    await user.keyboard('Ice skating with friends');
-    await user.click(eventdescField);
-    await user.keyboard('Meet up at the mall and go to the ice skating rink');
-    await user.click(eventdateField);
-    await user.keyboard('{Enter}'); // Simulates selecting the next day in the calendar component
-    await user.click(submitButton);
+    const { eventDateField, eventDescField, eventNameField } = setup();
+    const formFields = [eventNameField, eventDescField, eventDateField];
+    submitValidForm();
 
     formFields.forEach(field => {
       expect(field).not.toHaveValue();
