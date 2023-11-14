b1f3ad4e71571d57d16a542fafa1177d5f9db437
Carl Dungca <carl.dungca@yahoo.com>
2023-11-13 23:04:04 -0800

    feature: adds event not found route

--- app/dashboard/event/[id]/page.tsx
+++ app/dashboard/event/[id]/page.tsx
@@ -1,16 +1,23 @@
 'use client';
 import { EventContext } from '@/components/custom/DashboardWrapper';
 import { format } from 'date-fns';
+import { notFound } from 'next/navigation';
 import React, { useContext } from 'react';
 
 const EventPage = ({ params }: { params: { id: string } }) => {
   const { events } = useContext(EventContext);
   let filteredEvent = events.filter(event => event.id === parseInt(params.id));
+
+  if (filteredEvent.length < 1) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>{filteredEvent[0].title}</h1>
       <p>{format(filteredEvent[0].date, 'PPP')}</p>
       <p>{filteredEvent[0].body}</p>
+      <p>Guest list:</p>
     </div>
   );
 };
