ddc1a0caba4f6d58cf4e34a17a900075e89ff08b
Carl Dungca <carl.dungca@yahoo.com>
2023-11-16 00:00:57 -0800

    chore: removes console messages when setting up tests

--- setupTests.ts
+++ setupTests.ts
@@ -4,7 +4,3 @@ import { server } from '@/app/mocks/server';
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
-
-server.events.on('request:start', ({ request }) => {
-  console.log('MSW Intercepted:', request.method, request.url);
-});
