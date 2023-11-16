e2b24ebb6aa4642813dda2cc56565855dc886bff
Carl Dungca <carl.dungca@yahoo.com>
2023-11-16 01:27:08 -0800

    chore: regenerates database types

---
+++ types/database.types.ts
@@ -0,0 +1,47 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      events: {
+        Row: {
+          body: string | null
+          date: string | null
+          id: string
+          title: string | null
+        }
+        Insert: {
+          body?: string | null
+          date?: string | null
+          id: string
+          title?: string | null
+        }
+        Update: {
+          body?: string | null
+          date?: string | null
+          id?: string
+          title?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
