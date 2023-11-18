a52d1637ad2ac0c7a3162d96d9608b4fa53d3e8a
Carl Dungca <carl.dungca@yahoo.com>
2023-11-17 17:57:49 -0800

    refactor: changes name of navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/custom/Navbar';
+import Navbar from '@/components/custom/navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
