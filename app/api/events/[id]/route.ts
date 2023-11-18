e54c306b5ca04a7dddca43bdd4798ec5c36eab5b
Carl Dungca <carl.dungca@yahoo.com>
2023-11-17 18:24:01 -0800

    feature: uses cookies to handle supabase sessions

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,16 +1,35 @@
-import { createClient } from '@supabase/supabase-js';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { Database } from '@/types/database.types';
 import { Key } from 'react';
 import { NextResponse } from 'next/server';
 import EventType from '@/types/EventType';
+import { cookies } from 'next/headers';
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+function initSupabaseClient() {
+  const cookieStore = cookies();
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options });
+        },
+      },
+    }
+  );
+  return supabase;
+}
 
 // Edit an event with ID
 export async function PUT(request: Request) {
+  const supabase = initSupabaseClient();
   const reqData: EventType = await request.json();
   // Need to get event using ID
   const { data, error } = await supabase
@@ -28,6 +47,7 @@ export async function PUT(request: Request) {
 
 // Delete an event with ID
 export async function DELETE(request: Request) {
+  const supabase = initSupabaseClient();
   const reqData: Key = await request.json();
   const { data, error } = await supabase
     .from('events')
--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,15 +1,34 @@
-import { createClient } from '@supabase/supabase-js';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { Database } from '@/types/database.types';
 import EventType from '@/types/EventType';
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+function initSupabaseClient() {
+  const cookieStore = cookies();
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options });
+        },
+      },
+    }
+  );
+  return supabase;
+}
 
 // Grab API data
 export async function GET() {
+  const supabase = initSupabaseClient();
   const { data, error } = await supabase.from('events').select('*');
   if (error) return NextResponse.error();
   return NextResponse.json(data, { status: 200 });
@@ -17,6 +36,7 @@ export async function GET() {
 
 // Create new event data
 export async function POST(request: Request) {
+  const supabase = initSupabaseClient();
   const reqData: EventType = await request.json();
   const { data, error } = await supabase
     .from('events')
