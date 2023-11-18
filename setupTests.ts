e050446073990cbb03f07c227cf9fee2b19768ea
Carl Dungca <carl.dungca@yahoo.com>
2023-11-17 20:35:54 -0800

    config: sets dotenv to run before tests

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,6 @@
 import { beforeAll, afterEach, afterAll } from 'vitest';
 import { server } from '@/app/mocks/server';
+import 'dotenv/config';
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     css: true,
-    setupFiles: ['./setupTests.ts'],
+    setupFiles: ['./setupTests.ts', 'dotenv/config'],
   },
 });
