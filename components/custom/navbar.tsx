438d5bb52bd3934b6e3af6f80dc3597bc3789677
Carl Dungca <carl.dungca@yahoo.com>
2023-11-17 17:55:51 -0800

    bugfix: restores navbar from bad commit

---
+++ components/custom/navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+const Navbar = () => {
+  return (
+    <nav className="grid grid-cols-[1fr_auto_auto] gap-4 items-center p-4 bg-stone-950 text-white">
+      <b>PartyCraft</b>
+      {/* <span className="text-right">Signup</span> */}
+      {/* <span className="text-right">Login</span> */}
+      {/* <Avatar>
+				<AvatarImage src="#" alt="Test" />
+				<AvatarFallback className="text-black">CD</AvatarFallback>
+			</Avatar>
+				<span className="text-right">Logout</span> */}
+    </nav>
+  );
+};
+
+export default Navbar;
