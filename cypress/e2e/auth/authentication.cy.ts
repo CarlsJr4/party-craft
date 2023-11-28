5b5d47b3802d2b24a7cf71b77bb41d288a5deb6d
Carl Dungca <carl.dungca@yahoo.com>
2023-11-27 23:27:28 -0800

    refactor: moves protected routes tests to authorization.cy.ts

--- cypress/e2e/auth/authentication.cy.ts
+++ cypress/e2e/auth/authentication.cy.ts
@@ -34,17 +34,3 @@ describe('The login page', () => {
     cy.contains('Invalid email or password').should('not.exist');
   });
 });
-
-describe('protected routes', () => {
-  it('Redirects user to login page if accessing dashboard with no session', () => {
-    cy.visit('/dashboard/explore');
-    cy.getCookie('sb-localhost-auth-token').should('not.exist');
-    cy.contains('Email:').should('exist');
-  });
-
-  it('Redirects user to login page if accessing event with no session', () => {
-    cy.visit('/dashboard/event/385a0208-193f-5ece-8a97-b78aa8c9cea8');
-    cy.getCookie('sb-localhost-auth-token').should('not.exist');
-    cy.contains('Email:').should('exist');
-  });
-});
