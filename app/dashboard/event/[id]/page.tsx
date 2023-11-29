70fd64dc039801a02ced125483d1ba8e44eaa674
Carl Dungca <carl.dungca@yahoo.com>
2023-11-29 00:25:17 -0800

    feature: adds simple sign-up and cancel sign-up

--- app/dashboard/event/[id]/page.tsx
+++ app/dashboard/event/[id]/page.tsx
@@ -9,14 +9,20 @@ import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types/database.types';
 
 const EventPage = ({ params }: { params: { id: string } }) => {
+  // NOTE: This type is based off the profiles table
   type guestListType = {
+    id: string | null; // Represents user uuid in DB
     email: string | null;
-    id: string;
     role: string | null;
   };
 
   const { events } = useContext(EventContext);
   const [guestList, setGuestList] = useState<guestListType[]>([]);
+
+  const supabase = createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 
   useEffect(() => {
     async function fetchData() {
@@ -29,11 +35,6 @@ const EventPage = ({ params }: { params: { id: string } }) => {
     }
 
     async function fetchGuestList() {
-      const supabase = createBrowserClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-      );
-
       const { data: signupData, error: signupError } = await supabase
         .from('signups')
         .select()
@@ -55,12 +56,59 @@ const EventPage = ({ params }: { params: { id: string } }) => {
 
     fetchGuestList();
     fetchData();
-  }, [params.id]);
+  }, [params.id, supabase]);
   const [eventStatus, setEventStatus] = useState<EventType[] | null>([]);
 
   // notFound causes an error inside of useEffect so we call it here
   if (eventStatus === null) {
     notFound();
+  }
+
+  async function handleSignup() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    try {
+      const { data: guestData, error: guestDataError } = await supabase
+        .from('signups')
+        .insert({ event_id: params.id, user_id: user?.id })
+        .select();
+      if (guestDataError) {
+        throw new Error();
+      }
+      const { data: guestProfileData, error: guestProfileError } =
+        await supabase
+          .from('profiles')
+          .select()
+          .eq('id', guestData[0].user_id!);
+      const newGuestList = [...guestList];
+      if (guestProfileData !== null) {
+        const { email, id, role } = guestProfileData[0];
+        newGuestList.push({ email: email, id: id, role: role });
+        setGuestList(newGuestList);
+      }
+    } catch {
+      console.log('foo');
+    }
+    return;
+  }
+
+  async function handleCancelSignup() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    // TODO: Add delete RLS to signup table
+    if (user) {
+      const { error } = await supabase
+        .from('signups')
+        .delete()
+        .eq('event_id', params.id)
+        .eq('user_id', user?.id);
+      const updatedGuests = guestList.filter(guest => guest.id !== user?.id);
+      setGuestList(updatedGuests);
+    }
+    return;
   }
 
   let filteredEvent: EventType = events.filter(
@@ -73,11 +121,12 @@ const EventPage = ({ params }: { params: { id: string } }) => {
         <>
           <h1>{filteredEvent.title}</h1>
           <p>{format(new Date(filteredEvent.date), 'PPP')}</p>
-          <Button>Sign up</Button>
+          <Button onClick={() => handleSignup()}>Sign up</Button>
+          <Button onClick={() => handleCancelSignup()}>Cancel sign-up</Button>
           <p>{filteredEvent.body}</p>
           <p>Guest list:</p>
           {guestList.map(guest => (
-            <p key={guest.id}>{guest.role}</p>
+            <p key={guest.id}>{guest.email}</p>
           ))}
         </>
       ) : (
---
+++ cypress/e2e/auth/signups.cy.ts
@@ -0,0 +1,31 @@
+import { cy, Cypress, expect, it } from 'local-cypress';
+
+describe('event signups', () => {
+  it('Displays your name after a successful signup', () => {
+    cy.login('test@test.com', '111111');
+    cy.contains('Test future event').click();
+    cy.contains('Sign up').click();
+    cy.contains('test@test.com').should('exist');
+  });
+
+  it('Persists the signup from the previous test', () => {
+    cy.login('test@test.com', '111111');
+    cy.contains('Test future event').click();
+    cy.contains('test@test.com').should('exist');
+  });
+
+  it('Removes your name after clicking the cancel signup button', () => {
+    cy.login('test@test.com', '111111');
+    cy.contains('Test future event').click();
+    cy.contains('test@test.com').should('exist');
+    cy.contains('Cancel sign-up').should('exist');
+    cy.contains('Cancel sign-up').click();
+    cy.contains('test@test.com').should('not.exist');
+  });
+
+  it('Persists the delete from the previous test', () => {
+    cy.login('test@test.com', '111111');
+    cy.contains('Test future event').click();
+    cy.contains('test@test.com').should('not.exist');
+  });
+});
