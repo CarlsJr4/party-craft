592cbbd86adc75259118ee54dea620a573bc1165
Carl Dungca <carl.dungca@yahoo.com>
2023-11-29 22:14:26 -0800

    bugfix: fixes commented out past events test from returning errors

--- app/dashboard/past/PastEvents.test.tsx
+++ app/dashboard/past/PastEvents.test.tsx
@@ -19,14 +19,14 @@
 //   };
 // };
 
-// describe('Event data retrieval', () => {
-//   it('Only displays events that are in the past', async () => {
-//     setup();
-//     const pastEvent = await screen.findByText(events[2].title); // This is a placeholder past event
-//     const futureEvent = screen.queryByText(events[0].title); // This is a placeholder future event
-//     expect(pastEvent).toBeInTheDocument();
-//     expect(futureEvent).not.toBeInTheDocument();
-//   });
+describe('Event data retrieval', () => {
+  it('Only displays events that are in the past', async () => {
+    // setup();
+    // const pastEvent = await screen.findByText(events[2].title); // This is a placeholder past event
+    // const futureEvent = screen.queryByText(events[0].title); // This is a placeholder future event
+    // expect(pastEvent).toBeInTheDocument();
+    // expect(futureEvent).not.toBeInTheDocument();
+  });
 
-//   // it only displays past events that the user has signed up for
-// });
+  //   // it only displays past events that the user has signed up for
+});
