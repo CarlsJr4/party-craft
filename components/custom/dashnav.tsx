9d8138119fbffa547eff900fb9d728d723d79ffc
Carl Dungca <carl.dungca@yahoo.com>
2023-12-01 21:20:59 -0800

    feature: responsive sidebar

--- components/custom/dashnav.tsx
+++ components/custom/dashnav.tsx
@@ -6,19 +6,19 @@ import { HeartIcon, RocketIcon } from 'lucide-react';
 
 const DashNav = () => {
   return (
-    <div className="2xl:col-span-1 col-span-2 p-6 bg-slate-100 flex flex-col gap-6">
+    <div className="2xl:col-span-1 col-span-2 p-6 bg-slate-100 flex flex-col gap-6 items-center sm:items-stretch">
       <CreateDialog>
         <Button size="lg" className="mt-3">
-          Create New +
+          <span className="hidden sm:inline">Create New {'  '}</span>+
         </Button>
       </CreateDialog>
       <Link href="/dashboard/explore" className="flex items-center gap-3">
         <RocketIcon className="h-4 w-4" />
-        <span>Explore</span>
+        <span className="hidden sm:inline">Explore</span>
       </Link>
       <Link href="/dashboard/myevents" className="flex items-center gap-3">
         <HeartIcon className="h-4 w-4" />
-        <span>My Events</span>
+        <span className="hidden sm:inline">My Events</span>
       </Link>
       {/* <Link href="/dashboard/past">Past Events</Link> */}
       {/* <Link href="/dashboard/drafts">Drafts</Link> */}
