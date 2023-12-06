685b4f52db6aa69f25501df8395a7793fd3448a1
Carl Dungca <carl.dungca@yahoo.com>
2023-12-05 18:06:27 -0800

    feature: changes appearance of create button

--- components/custom/Dashnav.tsx
+++ components/custom/Dashnav.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import CreateDialog from '@/components/custom/CreateDialog';
-import { HeartIcon, RocketIcon } from 'lucide-react';
+import { HeartIcon, PlusCircleIcon, RocketIcon } from 'lucide-react';
 
 const Dashnav = () => {
   return (
     <div className="2xl:col-span-1 col-span-2 p-6 bg-slate-100 flex flex-col gap-6 items-center sm:items-stretch">
       <CreateDialog>
-        <Button size="lg" className="mt-3">
+        <Button className="mt-3">
           <span className="hidden sm:inline">Create New</span>
-          <span className="ml-2">+</span>
+          <PlusCircleIcon className="h-4 w-4 sm:ml-2 text-white" />
         </Button>
       </CreateDialog>
       <Link href="/dashboard/explore" className="flex items-center gap-3">
