b33cd7637b4942d10a0e36bd884fd2f02111e310
Carl Dungca <carl.dungca@yahoo.com>
2023-12-05 18:45:16 -0800

    feature: responsive sticky dashnav

--- components/custom/Dashnav.tsx
+++ components/custom/Dashnav.tsx
@@ -6,23 +6,25 @@ import { HeartIcon, PlusCircleIcon, RocketIcon } from 'lucide-react';
 
 const Dashnav = () => {
   return (
-    <div className="2xl:col-span-1 col-span-2 p-6 bg-slate-100 flex flex-col gap-6 items-center sm:items-stretch">
-      <CreateDialog>
-        <Button className="mt-3 flex items-center">
-          <span className="hidden sm:inline">Create New</span>
-          <PlusCircleIcon className="h-4 w-4 sm:ml-2 mt-[2.2px] text-white" />
-        </Button>
-      </CreateDialog>
-      <Link href="/dashboard/explore" className="flex items-center gap-3">
-        <RocketIcon className="h-4 w-4" />
-        <span className="hidden sm:inline">Explore</span>
-      </Link>
-      <Link href="/dashboard/myevents" className="flex items-center gap-3">
-        <HeartIcon className="h-4 w-4" />
-        <span className="hidden sm:inline">My Events</span>
-      </Link>
-      {/* <Link href="/dashboard/past">Past Events</Link> */}
-      {/* <Link href="/dashboard/drafts">Drafts</Link> */}
+    <div className="bg-slate-100">
+      <div className="2xl:col-span-1 col-span-2 p-6 sticky top-[73px] flex flex-col gap-6 items-center lg:items-stretch">
+        <CreateDialog>
+          <Button className="mt-3 flex items-center">
+            <span className="hidden lg:inline">Create</span>
+            <PlusCircleIcon className="h-4 w-4 lg:ml-2 lg:mt-[2.2px] text-white" />
+          </Button>
+        </CreateDialog>
+        <Link href="/dashboard/explore" className="flex items-center gap-3">
+          <RocketIcon className="h-4 w-4" />
+          <span className="hidden lg:inline">Explore</span>
+        </Link>
+        <Link href="/dashboard/myevents" className="flex items-center gap-3">
+          <HeartIcon className="h-4 w-4" />
+          <span className="hidden lg:inline">My Events</span>
+        </Link>
+        {/* <Link href="/dashboard/past">Past Events</Link> */}
+        {/* <Link href="/dashboard/drafts">Drafts</Link> */}
+      </div>
     </div>
   );
 };
