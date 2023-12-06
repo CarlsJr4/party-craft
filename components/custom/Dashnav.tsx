510b69136ea69e47cf307836e75b7a5dde9ccd56
Carl Dungca <carl.dungca@yahoo.com>
2023-12-05 18:16:13 -0800

    style: repositions + icon on create button

--- components/custom/Dashnav.tsx
+++ components/custom/Dashnav.tsx
@@ -8,9 +8,9 @@ const Dashnav = () => {
   return (
     <div className="2xl:col-span-1 col-span-2 p-6 bg-slate-100 flex flex-col gap-6 items-center sm:items-stretch">
       <CreateDialog>
-        <Button className="mt-3">
+        <Button className="mt-3 flex items-center">
           <span className="hidden sm:inline">Create New</span>
-          <PlusCircleIcon className="h-4 w-4 sm:ml-2 text-white" />
+          <PlusCircleIcon className="h-4 w-4 sm:ml-2 mt-[2.5px] text-white" />
         </Button>
       </CreateDialog>
       <Link href="/dashboard/explore" className="flex items-center gap-3">
